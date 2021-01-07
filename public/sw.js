600eb6aa0579aa745a915aa5ce1b5b044ab34df2
Mitch <mitch.mathias96@gmail.com>
2021-01-06 22:21:47 -0500

    got it working

--- public/sw.js
+++ public/sw.js
@@ -1,31 +1,69 @@
-if ("serviceWorker" in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker
-            .regiser('index.js')
-            .then(reg => console.log('its working'))
-            .catch(err => console.log('error'))
-    })
-}
 const cacheName = 'v1';
 const DATA_CACHE = 'data-cache';
 
 const FILES_TO_CACHE = [
     "/",
-    "../models/transaction.js",
+    "/db.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "/index.html",
     "/index.js",
-    "/style.css"
-]
+    "/styles.css",
+];
 
 
-self.addEventListener('install', e => {
-    console.log('Installed');
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting()
+});
 
-    e.waitUntill(
-        caches.open(cacheName)
-            .then(cache => { cache.addAll(FILES_TO_CACHE) })
-            .then(() => self.skipWaiting())
-    )
-})
+
+// activate
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== cacheName && key !== DATA_CACHE) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", (event) => {
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+           caches.open(DATA_CACHE)
+           .then(cache => {
+               return fetch(event.request)
+                    .then(response => {
+                        //if it is goo it gets cloned and stores it in the cache
+                        if (response.status === 200) {
+                            cache.put(event.request.url, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(event.request);
+                    });
+            }).catch(err => console.log(err))
+        )
+        return;
+    }
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
